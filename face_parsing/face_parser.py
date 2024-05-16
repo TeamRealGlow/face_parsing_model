5ed8bada9146c6d1afb576c3b13d967744ce9685
00seungho <tmdgh9910@naver.com>
2024-05-16 20:12:52 +0900

    return class grayScale img

--- face_parsing/face_parser.py
+++ face_parsing/face_parser.py
@@ -3,7 +3,7 @@ import torch
 import os.path as osp
 import torchvision.transforms as transforms
 from PIL import Image
-
+import numpy as np
 class Parser():
     def __init__(self,cp):
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -22,20 +22,23 @@ class Parser():
             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
         ])
         img = Image.open(imgPath)
+        w, h = (img.width, img.height)
         image = img.resize((512, 512), Image.BILINEAR)
         image = to_tensor(image)
         image = torch.unsqueeze(image, 0)
-        return image
-    def _make_face_seg(self,image):
+        return image,w,h
+    def _make_face_seg(self,image,w,h):
         with torch.no_grad():
             image = image.to(self.device)
             y_pred = self.model(image)[0]
             parsing = y_pred.squeeze(0).cpu().numpy().argmax(0)
-            return parsing
+            parsingimage = Image.fromarray((parsing).astype(np.uint8))
+            parsingimage = parsingimage.resize((w, h))
+            return parsingimage
 
-    def out_parsing(self,imgPath):
-        image = self._preprocessing(imgPath)
-        parsing = self._make_face_seg(image)
+    def out_parsing(self, imgPath):
+        image, w, h = self._preprocessing(imgPath)
+        parsing = self._make_face_seg(image, w, h)
         return parsing
 
 if __name__ == '__main__':
